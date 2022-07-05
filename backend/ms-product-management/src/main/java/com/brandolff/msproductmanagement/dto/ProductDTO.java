6d799bf536f95bb7d7481e643bae58386d877296
kevinbrandolff <kevinbrandolff@gmail.com>
2022-07-05 17:05:42 -0300

    fix(ProductDTO, ProductImagesDTO):fixing dtos.

--- backend/ms-product-management/src/main/java/com/brandolff/msproductmanagement/dto/ProductDTO.java
+++ backend/ms-product-management/src/main/java/com/brandolff/msproductmanagement/dto/ProductDTO.java
@@ -1,5 +1,6 @@
 package com.brandolff.msproductmanagement.dto;
 
+import com.brandolff.msproductmanagement.entity.ProductEntity;
 import com.brandolff.msproductmanagement.enums.SizeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 @NoArgsConstructor
@@ -16,10 +18,24 @@ public class ProductDTO {
 
     private Integer id;
     private String name;
-    private Double value;
+    private Double price;
     private String description;
     private SizeEnum size;
     private List<CategoryDTO> categories;
     private List<ProductImagesDTO> imagesUrls;
+
+    public ProductDTO( ProductEntity productEntity ) {
+        this.id = productEntity.getId();
+        this.name = productEntity.getName();
+        this.price = productEntity.getPrice();
+        this.description = productEntity.getDescription();
+        this.size = productEntity.getSize();
+        this.categories = productEntity.getCategories() != null ? productEntity.getCategories().stream()
+                .map( (category) -> new CategoryDTO(category) )
+                .collect(Collectors.toList()) : null;
+        this.imagesUrls = productEntity.getImagesUrls() != null ? productEntity.getImagesUrls().stream()
+                .map( (image) -> new ProductImagesDTO(image) )
+                .collect(Collectors.toList()) : null;
+    }
 
 }
--- backend/ms-product-management/src/main/java/com/brandolff/msproductmanagement/dto/ProductImagesDTO.java
+++ backend/ms-product-management/src/main/java/com/brandolff/msproductmanagement/dto/ProductImagesDTO.java
@@ -1,5 +1,6 @@
 package com.brandolff.msproductmanagement.dto;
 
+import com.brandolff.msproductmanagement.entity.ProductImagesEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,5 +14,14 @@ public class ProductImagesDTO {
 
     private Integer id;
     private String imageUrl;
+
+    public ProductImagesDTO( ProductImagesEntity productImagesEntity ) {
+        this.id = productImagesEntity.getId();
+        this.imageUrl = productImagesEntity.getImageUrl();
+    }
+
+    public ProductImagesDTO( String url ) {
+        this.imageUrl = url;
+    }
 
 }
