b347b239fac4994e5df9d7b2d2228637df4d1bc7
kevinbrandolff <kevinbrandolff@gmail.com>
2022-07-06 22:54:09 -0300

    feat(schema):adding schema.sql

---
+++ backend/ms-product-management/src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+create table if not exists category_entity (
+        id int not null primary key AUTO_INCREMENT,
+        category varchar(255) not null,
+)
+
+create table if not exists product_entity (
+        id int not null primary key AUTO_INCREMENT,
+        description varchar(255) not null,
+        name varchar(255) not null,
+        price double not null,
+        size enum('SMALL', 'MEDIUM', 'LARGE', 'XLARGE', 'XXLARGE'),
+)
+
+create table if not exists product_entity_category (
+        product_id integer not null,
+        category_id integer not null,
+        FOREIGN KEY (product_id) REFERENCES product_entity(id),
+        FOREIGN KEY (category_id) REFERENCES category_entity(id)
+)
+
+create table if not exists product_image_entity (
+        id int not null primary key AUTO_INCREMENT,
+        image_url varchar(255),
+        product_id integer,
+        FOREIGN KEY (product_id) REFERENCES product_entity(id)
+)
