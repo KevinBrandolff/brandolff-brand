148a122294be3d27ea8c68b3216247659ed0c969
kevinbrandolff <kevinbrandolff@gmail.com>
2022-07-07 16:53:38 -0300

    feat(CategoryServiceImpl):improving create and update methods.

--- backend/ms-product-management/src/main/java/com/brandolff/msproductmanagement/service/impl/CategoryServiceImpl.java
+++ backend/ms-product-management/src/main/java/com/brandolff/msproductmanagement/service/impl/CategoryServiceImpl.java
@@ -2,7 +2,9 @@ package com.brandolff.msproductmanagement.service.impl;
 
 import com.brandolff.msproductmanagement.dto.CategoryDTO;
 import com.brandolff.msproductmanagement.entity.CategoryEntity;
+import com.brandolff.msproductmanagement.exception.category.CategoryPersistException;
 import com.brandolff.msproductmanagement.exception.generic.ResourceNotFoundException;
+import com.brandolff.msproductmanagement.exception.product.ProductPersistException;
 import com.brandolff.msproductmanagement.repository.CategoryRepository;
 import com.brandolff.msproductmanagement.service.CategoryService;
 import org.springframework.beans.BeanUtils;
@@ -23,6 +25,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryDTO createCategory(CategoryDTO categoryDTO) {
+        verifyCategory( categoryDTO.getCategory() );
         CategoryEntity categoryEntity = CategoryEntity.builder().build();
         BeanUtils.copyProperties( categoryDTO, categoryEntity );
         return new CategoryDTO( repository.save( categoryEntity ) );
@@ -34,9 +37,13 @@ public class CategoryServiceImpl implements CategoryService {
         AtomicReference<CategoryEntity> categoryEntityUpdated = new AtomicReference<>(CategoryEntity.builder().build());
 
         repository.findById( categoryDTO.getId() ).ifPresentOrElse(
-                (categoryEntity) -> { BeanUtils.copyProperties( categoryDTO, categoryEntity );
-                                      categoryEntityUpdated.set(repository.save(categoryEntity));
-                                    },
+                (categoryEntity) -> {
+                    if ( !categoryDTO.getCategory().equals( categoryEntity.getCategory() ) ) {
+                        verifyCategory( categoryDTO.getCategory() );
+                    }
+                    BeanUtils.copyProperties( categoryDTO, categoryEntity );
+                    categoryEntityUpdated.set(repository.save(categoryEntity));
+                    },
                 () -> {
                     throw new ResourceNotFoundException();
                 }
@@ -58,5 +65,9 @@ public class CategoryServiceImpl implements CategoryService {
 
     private List<CategoryDTO> convertListEntityToDTO( List<CategoryEntity> listEntity ) {
         return listEntity.stream().map( categoryEntity -> new CategoryDTO(categoryEntity) ).collect(Collectors.toList());
+    }
+
+    private void verifyCategory( String category ) {
+        repository.findByCategory( category ).ifPresent( productEntity -> {throw new CategoryPersistException( "Category name already used!" );} );
     }
 }
